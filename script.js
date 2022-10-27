e6935e3c5d4e8e175d1700f50010ea583d579fe7
Luciano Coelho <coelho.luciano@icloud.com>
2022-10-26 21:55:25 -0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let isIgnite = true
 function changeCard(event) {
     const card = event.currentTarget 
-    const bg = isIgnite 
-    ? "ignite"
-    : "explorer" 
+    const bg = isIgnite ? "ignite" : "explorer" 
     isIgnite = !isIgnite
     card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
 }
